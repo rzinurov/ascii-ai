74ca7441881d3a9641fa72f001aa0e9615113ec0
Rustam Zinurov <rzinurov@gmail.com>
2024-08-25 23:59:32 +0200

    Calculate image width and height manually
    to make removal of image2ascii library simpler in future

--- ui/canvas.go
+++ ui/canvas.go
@@ -13,6 +13,8 @@ type Canvas struct {
 	image image.Image
 }
 
+var terminalSymbolAspectRatio = 1.5
+
 func NewCanvas(screen tcell.Screen, x1, y1, x2, y2 int, style tcell.Style) *Canvas {
 	return &Canvas{
 		NewBaseComponent(screen, Boundaries{x1, y1, x2, y2}, style),
@@ -44,11 +46,36 @@ func (c *Canvas) renderImage() {
 
 func (c *Canvas) generateContent() string {
 	convertOptions := convert.DefaultOptions
-	convertOptions.FitScreen = true
+	convertOptions.FitScreen = false
 	convertOptions.Colored = false
+
+	imgWidth, imgHeight := c.calculateImageSize()
+
+	convertOptions.FixedWidth = imgWidth
+	convertOptions.FixedHeight = imgHeight
 
 	converter := convert.NewImageConverter()
 	content := converter.Image2ASCIIString(c.image, &convertOptions)
 
 	return content
+}
+
+func (c *Canvas) calculateImageSize() (width, height int) {
+	imgBounds := c.image.Bounds()
+	imgWidth := float64(imgBounds.Size().X) * terminalSymbolAspectRatio
+	imgHeight := float64(imgBounds.Size().Y)
+	imgAspectRatio := imgWidth / imgHeight
+
+	screenWidth := float64(c.getWidth())
+	screenHeight := float64(c.getHeight())
+	screenAspectRatio := screenWidth / screenHeight
+
+	scaleRatio := 1.0
+	if imgAspectRatio >= screenAspectRatio {
+		scaleRatio = screenWidth / imgWidth
+	} else {
+		scaleRatio = screenHeight / imgHeight
+	}
+
+	return int(imgWidth * scaleRatio), int(imgHeight * scaleRatio)
 }
